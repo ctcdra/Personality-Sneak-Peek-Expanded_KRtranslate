9c4b4414de36aff75700e5895f4d8cffb654ae63
ctcdra <162713638+ctcdra@users.noreply.github.com>
2024-04-25 19:24:09 +0900

    정화의 성격 추가

--- dict/personalitydict.js
+++ dict/personalitydict.js
@@ -1,323 +1,323 @@
 const personalityJson = {
-	"[None]" : {
-		"back" : "",
-		"front" : ""
-	},
-	"Abundant" : {
-		"back" : "aura_e_emart",
-		"front" : "aura_e_emart"
-	},
-	"Abyssal" : {
-		"back" : "aura_s_abyss",
-		"front" : "aura_s_abyss"
-	},
-	"Adamant" : {
-		"back" : "aura_b_navy",
-		"front" : ""
-	},
-	"Affable" : {
-		"back" : "aura_s_xmas_b",
-		"front" : "aura_s_xmas_b"
-	},
-	"Apocalyptic" : {
-		"back" : "aura_s_end",
-		"front" : "aura_s_end"
-	},
-	"Arrogant" : {
-		"back" : "aura_a_red",
-		"front" : "a"
-	},
-	"Baleful" : {
-		"back" : "aura_s_dark",
-		"front" : "aura_s_dark"
-	},
-	"Bashful" : {
-		"back" : "aura_b_whight",
-		"front" : ""
-	},
-	"Benevolent" : {
-		"back" : "aura_a2_pink",
-		"front" : "a2"
-	},
-	"Blue Moon" : {
-		"back" : "aura_s_moon_blue",
-		"front" : "aura_s_transparent_blue"
-	},
-	"Blue Moon (Fly)" : {
-		"back" : "aura_s_moon_blue_fly",
-		"front" : "aura_s_transparent_blue_fly"
-	},
-	"Bold" : {
-		"back" : "aura_b_sky",
-		"front" : ""
-	},
-	"Brave" : {
-		"back" : "aura_b_red",
-		"front" : ""
-	},
-	"Calm" : {
-		"back" : "aura_b_brown",
-		"front" : ""
-	},
-	"Capable" : {
-		"back" : "aura_s_bluelight",
-		"front" : "aura_s_bluelight"
-	},
-	"Careful" : {
-		"back" : "aura_b_black",
-		"front" : ""
-	},
-	"Chaotic" : {
-		"back" : "aura_s_blackblue",
-		"front" : "aura_s_blackblue"
-	},
-	"Charming" : {
-		"back" : "aura_a_yellow",
-		"front" : "a"
-	},
-	"Cheerful" : {
-		"back" : "aura_a2_orange",
-		"front" : "a2"
-	},
-	"Christmas" : {
-		"back" : "aura_s_xmas_a",
-		"front" : "aura_s_xmas_a"
-	},
-	"Classy" : {
-		"back" : "aura_s_pearl",
-		"front" : "aura_s_pearl"
-	},
-	"Clever" : {
-		"each" : "aura_s_smart",
-		"back" : "back",
-		"front" : "front"
-	},
-	"Clumsy" : {
-		"back" : "aura_a2_purple",
-		"front" : "a2"
-	},
-	"Curious" : {
-		"back" : "aura_a2_green",
-		"front" : "a2"
-	},
-	"Dawn" : {
-		"back" : "aura_a2_white",
-		"front" : "a2_whitegold"
-	},
-	"Distracted" : {
-		"back" : "aura_s_mint",
-		"front" : "aura_s_champion2"
-	},
-	"Docile" : {
-		"back" : "aura_b_green",
-		"front" : ""
-	},
-	"Dull" : {
-		"back" : "aura_a_babyblue",
-		"front" : "a"
-	},
-	"Elegant" : {
-		"back" : "aura_s_blackhole",
-		"front" : "aura_s_star"
-	},
-	"Extroverted" : {
-		"back" : "aura_s_prism",
-		"front" : "aura_s_prism"
-	},
-	"Fearful" : {
-		"back" : "aura_a_mint",
-		"front" : "a"
-	},
-	"Frail" : {
-		"back" : "aura_s_blackgreen",
-		"front" : "aura_s_blackgreen"
-	},
-	"Full Moon" : {
-		"back" : "aura_s_moon_yellow",
-		"front" : "aura_s_transparent_yellow"
-	},
-	"Full Moon (Fly)" : {
-		"back" : "aura_s_moon_yellow_fly",
-		"front" : "aura_s_transparent_yellow_fly"
-	},
-	"Glorious": {
-		"back" : "aura_s_pvp_b",
-		"front" : "aura_s_pvp_b"
-	},
-	"Gluttonous": {
-		"back" : "aura_s_greedy",
-		"front" : "aura_s_greedy"
-	},
-	"Hardy" : {
-		"back" : "aura_b_darkbrown",
-		"front" : ""
-	},
-	"Hasty" : {
-		"back" : "aura_b_orangered",
-		"front" : ""
-	},
-	"Honorable": {
-		"back" : "aura_s_pvp_a",
-		"front" : "aura_s_pvp_a"
-	},
-	"Horrendous" : {
-		"back" : "aura_s_halloween_b",
-		"front" : "aura_s_halloween_b"
-	},
-	"Horrendous (Fly)" : {
-		"back" : "aura_s_halloween_b_fly",
-		"front" : "aura_s_halloween_b"
-	},
-	"Immersed" : {
-		"back" : "aura_s_universe2",
-		"front" : "aura_s_universe2"
-	},
-	"Innocent" : {
-		"each" : "aura_s_innocent",
-		"back" : "back",
-		"front" : "front"
-	},
-	"Introvert" : {
-		"back" : "aura_a2_red",
-		"front" : "a2"
-	},
-	"Judgmental": {
-		"each": "aura_s_judgement",
-		"back": "back",
-		"front": "front"
-	},
-	"Lax" : {
-		"back" : "aura_b_pink",
-		"front" : ""
-	},
-	"Lonely" : {
-		"back" : "aura_a_lavender",
-		"front" : "a"
-	},
-	"Lousy" : {
-		"back" : "aura_a_navypurple",
-		"front" : "a3_purple"
-	},
-	"Lovely" : {
-		"back" : "aura_a_babypink",
-		"front" : "a"
-	},
-	"Lunatic" : {
-		"back" : "aura_s_purplelight",
-		"front" : "aura_s_purplelight"
-	},
-	"Meticulous" : {
-		"back" : "aura_a_blacknavy",
-		"front" : "a3_blue"
-	},
-	"Mischievous" : {
-		"each" : "aura_s_stardust",
-		"back" : "back",
-		"front" : "front"
-	},
-	"Mystical" : {
-		"back" : "aura_a2_greenmint",
-		"front" : "a2_l"
-	},
-	"Naive" : {
-		"back" : "aura_b_yellow",
-		"front" : ""
-	},
-	"Neat" : {
-		"back" : "aura_s_transparent3",
-		"front" : "aura_s_star"
-	},
-	"Noble" : {
-		"back" : "aura_s_star",
-		"front" : "aura_s_star2"
-	},
-	"Perfectionist" : {
-		"back" : "aura_s_universe1",
-		"front" : "aura_s_universe1"
-	},
-	"Pure" : {
-		"back" : "aura_s_transparent",
-		"front" : "aura_s_transparent"
-	},
-	"Purified": {
-		"each": "aura_s_purifying",
-		"back": "back",
-		"front": "front"
-	},
-	"Quick Witted" : {
-		"back" : "aura_b_yellowgreen",
-		"front" : ""
-	},
-	"Quiet" : {
-		"back" : "aura_b_blue",
-		"front" : ""
-	},
-	"Quirky" : {
-		"back" : "aura_b_purple",
-		"front" : ""
-	},
-	"Rash" : {
-		"back" : "aura_b_orange",
-		"front" : ""
-	},
-	"Serious" : {
-		"back" : "aura_a_lapislazuli",
-		"front" : "a"
-	},
-	"Silent" : {
-		"back" : "aura_a2_darkblue",
-		"front" : "a2"
-	},
-	"Skilled" : {
-		"back" : "aura_s_game_a",
-		"front" : "aura_s_game_a"
-	},
-	"Smart" : {
-		"back" : "aura_b_mint",
-		"front" : ""
-	},
-	"Sociable" : {
-		"back" : "aura_a_pastelpink",
-		"front" : "a3_purple"
-	},
-	"Solitary" : {
-		"back" : "aura_a2_warmgray",
-		"front" : "a2"
-	},
-	"Somber" : {
-		"back" : "aura_a2_brown",
-		"front" : "a2_copper"
-	},
-	"Spooky" : {
-		"back" : "aura_s_halloween_a",
-		"front" : "aura_s_halloween_a"
-	},
-	"Spooky (Fly)" : {
-		"back" : "aura_s_halloween_a_fly",
-		"front" : "aura_s_halloween_a"
-	},
-	"Talented" : {
-		"back" : "aura_s_game_b",
-		"front" : "aura_s_game_b"
-	},
-	"Timid" : {
-		"back" : "aura_a_orange",
-		"front" : "a"
-	},
-	"Tired" : {
-		"back" : "aura_a_skywhite",
-		"front" : "a3_skywhite"
-	},
-	"Unknown" : {
-		"back" : "aura_s_universe3",
-		"front" : "aura_s_universe3"
-	},
-	"Wild" : {
-		"back" : "aura_s_blackred",
-		"front" : "aura_s_blackred"
-	},
-	"Wilderness" : {
-		"back" : "aura_s_sunset",
-		"front" : "aura_s_sunset"
+	"[없음]":{
+		"back":"",
+		"front":""
+	},
+	"겁이 많은":{
+        	"back":"aura_a_mint",
+        	"front":"a"
+    	},
+	"고귀한":{
+	        "back":"aura_s_star",
+	        "front":"aura_s_star2"
+	},
+	"고독한":{
+	        "back":"aura_a2_warmgray",
+	        "front":"a2"
+	},
+	"고집있는":{
+	        "back":"aura_b_navy",
+	        "front":""
+	},
+	"광적인":{
+	        "back":"aura_s_purplelight",
+	        "front":"aura_s_purplelight"
+	},
+	"기품있는":{
+	        "back":"aura_s_blackhole",
+	        "front":"aura_s_star"
+	},
+	"깨끗한":{
+	        "back":"aura_s_transparent3",
+	        "front":"aura_s_star"
+	},
+	"내향적인":{
+	        "back":"aura_a2_red",
+	        "front":"a2"
+	},
+	"냉정한":{
+	        "back":"aura_b_blue",
+	        "front":""
+	},
+	"노력하는":{
+	        "back":"aura_b_darkbrown",
+	        "front":""
+	},
+	"눈치빠른":{
+	        "back":"aura_b_yellowgreen",
+	        "front":""
+	},
+	"대담한":{
+	        "back":"aura_b_sky",
+	        "front":""
+	},
+	"덜렁대는":{
+	        "back":"aura_b_orange",
+	        "front":""
+	},
+	"따뜻한":{
+	        "back":"aura_s_xmas_b",
+	        "front":"aura_s_xmas_b"
+	},
+	"똑똑한":{
+	        "back":"aura_b_mint",
+	        "front":""
+	},
+	"명랑한":{
+	        "back":"aura_a2_orange",
+	        "front":"a2"
+	},
+	"명예로운":{
+	        "back":"aura_s_pvp_a",
+	        "front":"aura_s_pvp_a"
+	},
+	"몰입하는":{
+	        "back":"aura_s_universe2",
+	        "front":"aura_s_universe2"
+	},
+	"미지의":{
+	        "back":"aura_s_universe3",
+	        "front":"aura_s_universe3"
+	},
+	"변덕쟁이":{
+	        "back":"aura_b_purple",
+	        "front":""
+	},
+	"보름달의":{
+	        "back":"aura_s_moon_yellow",
+	        "front":"aura_s_transparent_yellow"
+	},
+	"보름달의 (비행)":{
+	        "back":"aura_s_moon_yellow_fly",
+	        "front":"aura_s_transparent_yellow_fly"
+	},
+	"부족한":{
+	        "back":"aura_a_navypurple",
+	        "front":"a3_purple"
+	},
+	"사교적인":{
+	        "back":"aura_a_pastelpink",
+	        "front":"a3_purple"
+	},
+	"사나운":{
+	        "back":"aura_s_blackred",
+	        "front":"aura_s_blackred"
+	},
+	"사랑스러운":{
+	        "back":"aura_a_babypink",
+	        "front":"a"
+	},
+	"산만한":{
+	        "back":"aura_s_mint",
+	        "front":"aura_s_champion2"
+	},
+	"서투른":{
+	        "back":"aura_a2_purple",
+	        "front":"a2"
+	},
+	"섬뜩한":{
+	        "back":"aura_s_halloween_b",
+	        "front":"aura_s_halloween_b"
+	},
+	"섬뜩한 (비행)":{
+	        "back":"aura_s_halloween_b_fly",
+	        "front":"aura_s_halloween_b"
+	},
+	"성급한":{
+	        "back":"aura_b_orangered",
+	        "front":""
+	},
+	"성실한":{
+	        "back":"aura_a_lapislazuli",
+	        "front":"a"
+	},
+	"성탄의":{
+	        "back":"aura_s_xmas_a",
+	        "front":"aura_s_xmas_a"
+	},
+	"소극적인":{
+	        "back":"aura_a_orange",
+	        "front":"a"
+	},
+	"수줍은":{
+	        "back":"aura_b_whight",
+	        "front":""
+	},
+	"순수한":{
+	        "back":"aura_s_transparent",
+	        "front":"aura_s_transparent"
+	},
+	"순진한" : {
+	        "each" : "aura_s_innocent",
+	        "back" : "back",
+	        "front" : "front"
+	},
+	"신비의":{
+	        "back":"aura_a2_greenmint",
+	        "front":"a2_l"
+	},
+	"신중한":{
+	        "back":"aura_b_black",
+	        "front":""
+	},
+	"실력좋은":{
+	        "back":"aura_s_game_a",
+	        "front":"aura_s_game_a"
+	},
+	"심약한":{
+	        "back":"aura_s_blackgreen",
+	        "front":"aura_s_blackgreen"
+	},
+	"심연의":{
+	        "back":"aura_s_abyss",
+	        "front":"aura_s_abyss"
+	},
+	"심판의": {
+	        "each": "aura_s_judgement",
+	        "back": "back",
+	        "front": "front"
+	},
+	"애교많은":{
+	        "back":"aura_a_yellow",
+	        "front":"a"
+	},
+	"어두운":{
+	        "back":"aura_a2_brown",
+	        "front":"a2_copper"
+	},
+	"여명의":{
+	        "back":"aura_a2_white",
+	        "front":"a2_whitegold"
+	},
+	"영광의":{
+	        "back":"aura_s_pvp_b",
+	        "front":"aura_s_pvp_b"
+	},
+	"영리한" : {
+	        "each" : "aura_s_smart",
+	        "back" : "back",
+	        "front" : "front"
+	},
+	"오만한":{
+	        "back":"aura_a_red",
+	        "front":"a"
+	},
+	"오싹한":{
+	        "back":"aura_s_halloween_a",
+	        "front":"aura_s_halloween_a"
+	},
+	"오싹한 (비행)":{
+	        "back":"aura_s_halloween_a_fly",
+	        "front":"aura_s_halloween_a"
+	},
+	"온순한":{
+	        "back":"aura_b_green",
+	        "front":""
+	},
+	"완벽주의자":{
+	        "back":"aura_s_universe1",
+	        "front":"aura_s_universe1"
+	},
+	"외로워하는":{
+	        "back":"aura_a_lavender",
+	        "front":"a"
+	},
+	"외향적인":{
+	        "back":"aura_s_prism",
+	        "front":"aura_s_prism"
+	},
+	"용감한":{
+	        "back":"aura_b_red",
+	        "front":""
+	},
+	"유능한":{
+	        "back":"aura_s_bluelight",
+	        "front":"aura_s_bluelight"
+	},
+	"자애로운" : {
+	        "back" : "aura_a2_pink",
+	        "front" : "a2"
+	},
+	"장난스러운" : {
+	        "each" : "aura_s_stardust",
+	        "back" : "back",
+	        "front" : "front"
+	},
+	"재능있는":{
+	        "back":"aura_s_game_b",
+	        "front":"aura_s_game_b"
+	},
+	"재앙의":{
+	        "back":"aura_s_dark",
+	        "front":"aura_s_dark"
+	},
+	"정화의": {
+	        "each": "aura_s_purifying",
+	        "back": "back",
+	        "front": "front"
+	},
+	"정확한":{
+	        "back":"aura_a_blacknavy",
+	        "front":"a3_blue"
+	},
+	"조용한":{
+	        "back":"aura_a2_darkblue",
+	        "front":"a2"
+	},
+	"종말의":{
+	        "back":"aura_s_end",
+	        "front":"aura_s_end"
+	},
+	"차분한":{
+	        "back":"aura_b_brown",
+	        "front":""
+	},
+	"천진난만한":{
+	        "back":"aura_b_yellow",
+	        "front":""
+	},
+	"촐랑대는":{
+	        "back":"aura_b_pink",
+	        "front":""
+	},
+	"탐식의":{
+	        "back":"aura_s_greedy",
+	        "front":"aura_s_greedy"
+	},
+	"평범한":{
+	        "back":"aura_a_babyblue",
+	        "front":"a"
+	},
+	"푸른달의":{
+	        "back":"aura_s_moon_blue",
+	        "front":"aura_s_transparent_blue"
+	},
+	"푸른달의 (비행)":{
+	        "back":"aura_s_moon_blue_fly",
+	        "front":"aura_s_transparent_blue_fly"
+	},
+	"품위있는":{
+	        "back":"aura_s_pearl",
+	        "front":"aura_s_pearl"
+	},
+	"풍요로운":{
+	        "back":"aura_e_emart",
+	        "front":"aura_e_emart"
+	},
+	"피곤한" : {
+	        "back" : "aura_a_skywhite",
+	        "front" : "a3_skywhite"
+	},
+	"호기심많은":{
+	        "back":"aura_a2_green",
+	        "front":"a2"
+	},
+	"혼돈의":{
+	        "back":"aura_s_blackblue",
+	        "front":"aura_s_blackblue"
+	},
+	"황야의":{
+	        "back":"aura_s_sunset",
+	        "front":"aura_s_sunset"
 	}
 }
