eb508108cd4d37018ff4221e33bf80924a2a8bc2
ctcdra <162713638+ctcdra@users.noreply.github.com>
2024-05-05 01:39:19 +0900

    Create customfrontdict.js

---
+++ dict/customfrontdict.js
@@ -0,0 +1,74 @@
+const customfrontJson = {
+    "[없음]" : {
+        "파란색": "",
+        "검정색": "",
+        "초록색": "",
+        "보라색": "",
+        "남색": "",
+        "분홍색": "",
+        "빨간색": "",
+        "스페셜": "",
+        "흰색": "",
+        "노란색": ""
+    },
+    "A" : {
+        "파란색": "front_a_b",
+        "검정색": "front_a_bk",
+        "초록색": "front_a_g",
+        "보라색": "front_a_p",
+        "남색": "front_a_pb",
+        "분홍색": "front_a_pk",
+        "빨간색": "front_a_r",
+        "스페셜": "front_a_spc",
+        "흰색": "front_a_w",
+        "노란색": "front_a_y"
+    },
+    "B": {
+        "파란색": "front_b_b",
+        "검정색": "front_b_bk",
+        "초록색": "front_b_g",
+        "보라색": "front_b_p",
+        "남색": "front_b_pb",
+        "분홍색": "front_b_pk",
+        "빨간색": "front_b_r",
+        "스페셜": "front_b_spc",
+        "흰색": "front_b_w",
+        "노란색": "front_b_y"
+    },
+    "C": {
+        "파란색": "front_c_b",
+        "검정색": "front_c_bk",
+        "초록색": "front_c_g",
+        "보라색": "front_c_p",
+        "남색": "front_c_pb",
+        "분홍색": "front_c_pk",
+        "빨간색": "front_c_r",
+        "스페셜": "front_c_spc",
+        "흰색": "front_c_w",
+        "노란색": "front_c_y"
+    },
+    "D": {
+        "파란색": "front_d_b",
+        "검정색": "front_d_bk",
+        "초록색": "front_d_g",
+        "보라색": "front_d_p",
+        "남색": "front_d_pb",
+        "분홍색": "front_d_pk",
+        "빨간색": "front_d_r",
+        "스페셜": "front_d_spc",
+        "흰색": "front_d_w",
+        "노란색": "front_d_y"
+    },
+    "E": {
+        "파란색": "front_e_b",
+        "검정색": "front_e_bk",
+        "초록색": "front_e_g",
+        "보라색": "front_e_p",
+        "남색": "front_e_pb",
+        "분홍색": "front_e_pk",
+        "빨간색": "front_e_r",
+        "스페셜": "front_e_spc",
+        "흰색": "front_e_w",
+        "노란색": "front_e_y"
+    },
+}
