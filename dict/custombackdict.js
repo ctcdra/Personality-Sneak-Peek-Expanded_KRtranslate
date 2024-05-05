5b1561a9c633a5a2a61e5771a4c69e8620c55123
ctcdra <162713638+ctcdra@users.noreply.github.com>
2024-05-05 14:39:59 +0900

    Create custombackdict.js

---
+++ dict/custombackdict.js
@@ -0,0 +1,74 @@
+const custombackJson = {
+    "[없음]" : {
+        "파란색": "",
+        "검정색": "",
+        "초록색": "",
+        "보라색": "",
+        "남색": "",
+        "분홍색": "",
+        "빨간색": "",
+        "스페셜": "",
+        "흰색": "",
+        "노란색": ""
+    },
+    "A": {
+        "파란색": "back_a_b",
+        "검정색": "back_a_bk",
+        "초록색": "back_a_g",
+        "보라색": "back_a_p",
+        "남색": "back_a_pb",
+        "분홍색": "back_a_pk",
+        "빨간색": "back_a_r",
+        "스페셜": "back_a_spc",
+        "흰색": "back_a_w",
+        "노란색": "back_a_y"
+    },
+    "B": {
+        "파란색": "back_b_b",
+        "검정색": "back_b_bk",
+        "초록색": "back_b_g",
+        "보라색": "back_b_p",
+        "남색": "back_b_pb",
+        "분홍색": "back_b_pk",
+        "빨간색": "back_b_r",
+        "스페셜": "back_b_spc",
+        "흰색": "back_b_w",
+        "노란색": "back_b_y"
+    },
+    "C": {
+        "파란색": "back_c_b",
+        "검정색": "back_c_bk",
+        "초록색": "back_c_g",
+        "보라색": "back_c_p",
+        "남색": "back_c_pb",
+        "분홍색": "back_c_pk",
+        "빨간색": "back_c_r",
+        "스페셜": "back_c_spc",
+        "흰색": "back_c_w",
+        "노란색": "back_c_y"
+    },
+    "D": {
+        "파란색": "back_d_b",
+        "검정색": "back_d_bk",
+        "초록색": "back_d_g",
+        "보라색": "back_d_p",
+        "남색": "back_d_pb",
+        "분홍색": "back_d_pk",
+        "빨간색": "back_d_r",
+        "스페셜": "back_d_spc",
+        "흰색": "back_d_w",
+        "노란색": "back_d_y"
+    },
+    "E": {
+        "파란색": "back_e_b",
+        "검정색": "back_e_bk",
+        "초록색": "back_e_g",
+        "보라색": "back_e_p",
+        "남색": "back_e_pb",
+        "분홍색": "back_e_pk",
+        "빨간색": "back_e_r",
+        "스페셜": "back_e_spc",
+        "흰색": "back_e_w",
+        "노란색": "back_e_y"
+    },
+}
