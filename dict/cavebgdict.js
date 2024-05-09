c12baf54e3341360bd7026741077ef7a4796eeeb
ctcdra <162713638+ctcdra@users.noreply.github.com>
2024-05-09 12:48:46 +0900

    Create cavebgdict.js

---
+++ dict/cavebgdict.js
@@ -0,0 +1,74 @@
+const cavebgJson = {
+    "[없음]": {
+        "res": "none"
+    },
+    "종말의 왕국": {
+        "res": "kingdomoftheend/kingdomoftheend_bg_01/kingdomoftheend_bg_01"
+    },
+    "아쿠리스 심연": {
+        "res": "deepsea/deepsea_bg_01/deepsea_bg_01"
+    },
+    "아쿠리스 바닷속": {
+        "res": "shallowsea/shallowsea_bg_01/shallowsea_bg_01"
+    },
+    "아르카의 재판장": {
+        "res": "judge/judge_bg_01/judge_bg_01"
+    },
+    "아르카의 집행장": {
+        "res": "executor/executor_bg_01/executor_bg_01"
+    },
+    "클라비스" : {
+        "res": "clavis/clavis_bg_01/clavis_bg_01"
+    },
+    "클라비스 스페셜": {
+        "res": "clavis/clavis_bg_02/clavis_bg_02"
+    },
+    "까막나라": {
+        "res": "kkamaknara/kkamaknara_bg_01/kkamaknara_bg_01"
+    },
+    "에슈": {
+        "res": "eshu/eshu_bg_01/eshu_bg_01"
+    },
+    "페루나": {
+        "res": "feruna/feruna_bg_01/feruna_bg_01"
+    },
+    "페루나 스페셜": {
+        "res": "feruna/feruna_bg_02/feruna_bg_02"
+    },
+    "2023 할로윈": {
+        "res": "halloween_2023/halloween_2023_bg_01/halloween_2023_bg_01"
+    },
+    "야누스": {
+        "res": "janus/janus_bg_01/janus_bg_01"
+    },
+    "야누스 스페셜": {
+        "res": "janus/janus_bg_02/janus_bg_02"
+    },
+    "메티스": {
+        "res": "metis/metis_bg_01/metis_bg_01"
+    },
+    "메티스 스페셜": {
+        "res": "metis/metis_bg_02/metis_bg_02"
+    },
+    "Old Palace": {
+        "res": "korean_palace/korean_palace_bg_01/korean_palace_bg_01"
+    },
+    "푸솜": {
+        "res": "pusom/pusom_bg_01/pusom_bg_01"
+    },
+    "푸솜 스페셜": {
+        "res": "pusom/pusom_bg_02/pusom_bg_02"
+    },
+    /*"Ranky": {
+        "res": "ranky/ranky_bg_01/ranky_bg_01"
+    },*/
+    "바스카": {
+        "res": "vaska/vaska_bg_01/vaska_bg_01"
+    },
+    "바스카 스페셜": {
+        "res": "vaska/vaska_bg_02/vaska_bg_02"
+    },
+    /*"Xmas 2023": {
+        "res": "xmas_2023/xmas_2023_bg_01/xmas_2023_bg_01"
+    },*/
+}
