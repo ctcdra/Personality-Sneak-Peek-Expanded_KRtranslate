9857958713a2981e070ef4638082620b7de62011
ctcdra <162713638+ctcdra@users.noreply.github.com>
2024-05-09 12:51:28 +0900

    Create cavefloordict.js

---
+++ dict/cavefloordict.js
@@ -0,0 +1,77 @@
+const cavefloorJson = {
+    "[없음]": {
+        "res": "none"
+    },
+    "종말의 왕국": {
+        "res": "kingdomoftheend/kingdomoftheend_floor_01/kingdomoftheend_floor_01"
+    },
+    "아쿠리스 심연": {
+        "res": "deepsea/deepsea_floor_01/deepsea_floor_01"
+    },
+    "아쿠리스 바닷속": {
+        "res": "shallowsea/shallowsea_floor_01/shallowsea_floor_01"
+    },
+    "아르카의 재판장": {
+        "res": "judge/judge_floor_01/judge_floor_01"
+    },
+    "아르카의 집행장": {
+        "res": "executor/executor_floor_01/executor_floor_01"
+    },
+    "클라비스" : {
+        "res": "clavis/clavis_floor_01/clavis_floor_01"
+    },
+    "클라비스 스페셜": {
+        "res": "clavis/clavis_floor_02/clavis_floor_02"
+    },
+    "까막나라": {
+        "res": "kkamaknara/kkamaknara_floor_01/kkamaknara_floor_01"
+    },
+    "에슈": {
+        "res": "eshu/eshu_floor_01/eshu_floor_01"
+    },
+    "페루나": {
+        "res": "feruna/feruna_floor_01/feruna_floor_01"
+    },
+    "페루나 스페셜": {
+        "res": "feruna/feruna_floor_02/feruna_floor_02"
+    },
+    "할로윈 2023": {
+        "res": "halloween_2023/halloween_2023_floor_01/halloween_2023_floor_01"
+    },
+    "야누스": {
+        "res": "janus/janus_floor_01/janus_floor_01"
+    },
+    "야누스 스페셜": {
+        "res": "janus/janus_floor_02/janus_floor_02"
+    },
+    "루민디스": {
+        "res": "lumindis/lumindis_floor_01/lumindis_floor_01"
+    },
+    "메티스": {
+        "res": "metis/metis_floor_01/metis_floor_01"
+    },
+    "메티스 스페셜": {
+        "res": "metis/metis_floor_02/metis_floor_02"
+    },
+    "Old Palace": {
+        "res": "korean_palace/korean_palace_floor_01/korean_palace_floor_01"
+    },
+    "푸솜": {
+        "res": "pusom/pusom_floor_01/pusom_floor_01"
+    },
+    "푸솜 스페셜": {
+        "res": "pusom/pusom_floor_02/pusom_floor_02"
+    },
+    /*"Ranky": {
+        "res": "ranky/ranky_floor_01/ranky_floor_01"
+    },*/
+    "바스카": {
+        "res": "vaska/vaska_floor_01/vaska_floor_01"
+    },
+    "바스카 스페셜": {
+        "res": "vaska/vaska_floor_02/vaska_floor_02"
+    },
+    /*"Xmas 2023": {
+        "res": "xmas_2023/xmas_2023_floor_01/xmas_2023_floor_01"
+    },*/
+}
